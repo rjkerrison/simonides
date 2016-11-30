8da8cd525655a109727e0fa28f98735665e8544a
rjkerrison <rjk1994@gmail.com>
2016-11-30 00:02:19 +0000

    ReactJS.Net attempts

---
+++ Simonides/Scripts/HelloWorld.jsx
@@ -0,0 +1,44 @@
+﻿var TestCard = React.createClass({
+
+    getInitialState: function () {
+        return { data: '' };
+    },
+
+    componentWillMount: function () {
+        try {
+            var card = this.props.card;
+            this.data.imagesrc = card.image;
+            this.data.cardcode = card.cardcode;
+        }
+        catch (e) {
+            this.data = {
+                imagesrc: 'http://placehold.it/200x100',
+                cardcode: 'BS',
+                props: this.props,
+                exception: e
+            };
+        }
+    },
+
+    nextCard: function (e) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', '/Cards/NextTestCard/' + this.props.deck.deckid + '/' + (this.props.position + 1), true);
+        xhr.onload = function () {
+            var response = JSON.parse(xhr.responseText);
+
+            this.setState({ data: response.result });
+        }.bind(this);
+        xhr.send();
+    },
+
+    render: function () {
+        return (
+            <div class="card">
+                <img src="{this.data.imagesrc}" alt="{this.data.cardcode}" width="75" height="105" />
+                <div onClick="{this.nextCard}">
+                    NEXT
+                </div>
+            </div>
+            )
+    }
+});
