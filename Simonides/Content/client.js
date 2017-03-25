eb0fa4beb7277cddbffb8f40d13e2e74d165f897
rjkerrison <rjk1994@gmail.com>
2017-03-25 00:37:05 +0000

    .csproj and Content updates

---
+++ Simonides/Content/client.js
@@ -0,0 +1,4 @@
+﻿// All JavaScript in here will be loaded client-side
+var React = require('expose?React!react');
+var ReactDOM = require('expose?ReactDOM!react-dom');
+var Components = require('expose?Components!./components');
---
+++ Simonides/Content/components/index.js
@@ -0,0 +1,3 @@
+﻿module.exports = {
+    TestCard: require('./TestCard')
+}
